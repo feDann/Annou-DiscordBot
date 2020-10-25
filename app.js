796c482ee3f875f80a32fd24bb2119d990af5fb5
fedann <daniele99@ferrazzo.it>
2020-10-25 19:19:46 +0100

    listener for join action on voice channel

--- app.js
+++ app.js
@@ -7,11 +7,35 @@ const bot = new Discord.Client();
 
 const TOKEN = process.env.TOKEN;
 
+
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
   });
 
+bot.on('voiceStateUpdate',(oldMember , newMember ) =>{
+    
+    let newUserChannel = newMember.channel
+    let oldUserChannel = oldMember.channel
 
 
+    if(newUserChannel !== null && oldUserChannel !== newUserChannel ) {
+
+        const channelid = newMember.channelID;
+        const userid = newMember.id;
+        const username = newMember.member.displayName;
+
+        console.log(username + ' joined channel')
+
+    } else if(newUserChannel === null){
+
+        const username = newMember.member.displayName;
+
+        console.log(username + ' leaves channel')
+    }
+    else{
+        console.log('other stuff')
+    }
+    
+})
 
 bot.login(TOKEN);
