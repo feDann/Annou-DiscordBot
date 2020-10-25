1ec014450410f003ece09a102c1149de95280a2c
fedann <daniele99@ferrazzo.it>
2020-10-25 18:11:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.TOKEN;
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+  });
+
+
+
+
+bot.login(TOKEN);
