432c880cbc3c87bbfa55c6918aaaba7827da30ce
fedann <daniele99@ferrazzo.it>
2020-10-27 00:32:07 +0100

    fix a bug during tts generation

--- audiocommands/deafalert.js
+++ audiocommands/deafalert.js
@@ -5,12 +5,15 @@ module.exports = {
     name : 'deafalert',
     description : 'this command is autocalled when someone deaf himself',
     async execute(oldMember, newMember){
-        
+        try{
         const username = newMember.member.displayName;
         const connection = await newMember.channel.join();
         const stream = discordTTS.getVoiceStream(`${username} make himself deaf`);
         const dispatcher = connection.play(stream);       
-            
+        }
+        catch(err){
+            console.log('Error occured')
+        }    
                   
 
     }
--- audiocommands/mutedalert.js
+++ audiocommands/mutedalert.js
@@ -5,11 +5,14 @@ module.exports = {
     name : 'mutedalert',
     description : 'this command is autocalled when someone mute himself',
     async execute(oldMember, newMember){
-        
+        try{
         const username = newMember.member.displayName;
         const connection = await newMember.channel.join();
         const stream = discordTTS.getVoiceStream(`${username} muted himself`);
         const dispatcher = connection.play(stream);       
+        }catch(err){
+            console.log('Error occured')
+        }
             
                   
 
