c6a8219a46e1454419620ccc8597c8944b847c5d
fedann <daniele99@ferrazzo.it>
2020-10-31 19:44:51 +0100

    add embed message

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,16 @@
+const {MessageEmbed} = require('discord.js'); 
+
 module.exports = {
     name : 'help',
     description : 'Show the list of all commands',
     async execute(message, args){
         
         const helpmessage = "use the following command while you are in a voice  channel\n\n**!say [LANGUAGE CODE] [YOUR TEXT]** - Annou join your voice channel and say your text in the selected language\n\n**!alerton** - activate alert for mute action *ecc* \n\n**!alertoff** - deactivate alert for mute action *ecc* \n\n**!setsong [YOUTUBE_URL]** - when you join the current voice channel Annou  will play the chosen song for 10 seconds\n\n**!deletesong** - delete your song on join for the current voice channel\n\n**!stop** - stop the playing song\n\n**!leave** - Annou leaves your voice channel";
-
-        message.reply(helpmessage);
+        const embed = new MessageEmbed()
+        .setTitle('!help')
+        .setAuthor('Annou')
+        .setDescription(helpmessage)
+        .setColor('0xff0000');
+        message.reply(embed);
     }
 }
