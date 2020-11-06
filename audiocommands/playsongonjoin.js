e33d47d255e71251d6e1fe7ca0854e1dfdfe7a89
fedann <daniele99@ferrazzo.it>
2020-11-06 13:46:26 +0100

    improve playsong on join

--- audiocommands/playsongonjoin.js
+++ audiocommands/playsongonjoin.js
@@ -1,33 +1,29 @@
 const AudioOnJoin = require('../models/audioonjoin');
 const ytdl = require('ytdl-core');
 
-
 module.exports = {
-    name: 'playsongonjoin',
-    description: 'this command is autocalled when someone join a voice channel',
-    async execute(oldMember, newMember) {
+	name: 'playsongonjoin',
+	description: 'this command is autocalled when someone join a voice channel',
+	async execute(oldMember, newMember) {
+		const channelid = newMember.channelID;
+		const userid = newMember.id;
 
-        const channelid = newMember.channelID;
-        const userid = newMember.id;
+		const song = await AudioOnJoin.findOne({
+			channelid: channelid,
+			userid: userid,
+		});
 
-        const song = await AudioOnJoin.findOne({
-            "channelid": channelid,
-            "userid": userid
-        })
+		if (song) {
+			const connection = await newMember.channel.join();
+			const dispatcher = connection.play(
+				ytdl(song.url, { filter: 'audioonly' }),
+				{ seek: song.startTime ? song.startTime : '0' }
+			);
+			// ! Works but still in tersting!
+			setTimeout(() => {
+                dispatcher.destroy();
+			}, 10 * 1000);
 
-        if (song) {
-            const connection = await newMember.channel.join();
-            const dispatcher = connection.play(ytdl(song.url, { filter: 'audioonly' }), {seek:song.startTime ?song.startTime : '0'})            
-            
-
-            dispatcher.on('speaking',() => {
-                if(dispatcher.streamTime > (10 * 1000)){
-                    dispatcher.end();
-                    return;
-                }            
-            })
-
-
-    }
-}
-}
+		}
+	},
+};
